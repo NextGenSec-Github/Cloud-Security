63a28bf632eb1be3db259aded005d95b1ed0fbef
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-05 21:26:27 +0500

    Update awscli_redteam_cheatsheet.sh

--- aws/Offensive Security on AWS/awscli_redteam_cheatsheet.sh
+++ aws/Offensive Security on AWS/awscli_redteam_cheatsheet.sh
@@ -1,4 +1,30 @@
-# General Commands
+# ========================================
+# GENERAL & INFORMATION GATHERING COMMANDS
+# ========================================
+
+# List all ec2 instances
+aws ec2 describe-instances
+
+# List all s3 buckets
+aws s3 ls
+
+# List all iam users
+aws iam list-users
+
+# Retrieves the policy attached to a role
+
+# List all rds instances
+aws rds describe-db-instance
+
+# List all lambda functions
+aws lambda list-functions
 
 # List Delegated Service Administrators for Accounts Within Your AWS Organization
 organizations list-delegated-services-for-account --account-id <account-id> --profile <e.g. Production>
+
+# ====================
+# PRIVILEGE ESCALATION
+# ====================
+
+
+
