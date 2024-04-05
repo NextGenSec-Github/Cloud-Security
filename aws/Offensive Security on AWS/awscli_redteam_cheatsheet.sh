02e7c27dca716ad53134c604e34ec7bca5e77101
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-05 21:53:28 +0500

    Update awscli_redteam_cheatsheet.sh

--- aws/Offensive Security on AWS/awscli_redteam_cheatsheet.sh
+++ aws/Offensive Security on AWS/awscli_redteam_cheatsheet.sh
@@ -1,5 +1,5 @@
 # ========================================
-# GENERAL & INFORMATION GATHERING COMMANDS
+# RECONNAISSANCE & INFORMATION GATHERING COMMANDS
 # ========================================
 
 # List all ec2 instances
@@ -8,20 +8,26 @@ aws ec2 describe-instances
 # List all s3 buckets
 aws s3 ls
 
+# View the access policy of a bucket
+aws s3api get-bucket-policy --bucket <bucket-name>
+
 # List all iam users
 aws iam list-users
 
+# Get information about the user/role
+aws sts get-caller-identity
+
 # Retrieves the policy attached to a role
-aws iam get-role-policy
+aws iam get-role-policy --role-name <role-name> --policy-name <policy-name>
 
 # Retrieves the policy attached to a group
-aws iam get-group-policy
+aws iam get-group-policy --group-name <group-name> --policy-name <policy-name>
 
 # Retrieves the policy attached to a user
-aws iam get-user-policy
+aws iam get-user-policy --user-name <iam-username>
 
 # Lists policies attached to a role
-aws iam list-attached-role-policies
+aws iam list-attached-role-policies --role-name <role-name>
 
 # List all rds instances
 aws rds describe-db-instance
