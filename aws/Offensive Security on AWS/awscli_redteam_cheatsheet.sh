02dbf2fe65e836df0a64915897c8fa1561535f2d
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-05 21:15:07 +0500

    Update and rename aws_redteam_cheatsheet.sh to awscli_redteam_cheatsheet.sh

---
+++ aws/Offensive Security on AWS/awscli_redteam_cheatsheet.sh
@@ -0,0 +1,4 @@
+# General Commands
+
+# List Delegated Service Administrators for Accounts Within Your AWS Organization
+organizations list-delegated-services-for-account --account-id <account-id> --profile <e.g. Production>
