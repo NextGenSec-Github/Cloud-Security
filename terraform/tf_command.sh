f48a6e9112a13b52f0b98f19b88c351db4460e6b
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-08-19 21:51:18 +0500

    Update tf_command.sh

--- terraform/tf_command.sh
+++ terraform/tf_command.sh
@@ -6,6 +6,13 @@ terraform graph
 terraform output # List all defined outputs
 terraform output variable_name # Rtrieve the value of a specific output
 public_ip=$(terraform output -raw public_ip) # Putting output variables into env variables
+terraform workspace select # Switch to a new workspace
+terraform workspace show
+terraform workspace new example1
+terraform workspace new # Deploy a new copy of the exact same infrastructure, but store the state in a separate file
+export TF_VAR_db_username="(YOUR_DB_USERNAME)"
+export TF_VAR_db_password="(YOUR_DB_PASSWORD)" 
+
 
 # ==============
 # LOCAL GIT REPO
